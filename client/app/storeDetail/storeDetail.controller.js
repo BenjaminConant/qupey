6f0f5944228893d17b55536246a792ef1dbea92a
Ayana <ayana.d.i.wilson@gmail.com>
2015-04-17 16:39:56 -0400

    mostly done with nodemailer, just need to connect to front end

--- client/app/storeDetail/storeDetail.controller.js
+++ client/app/storeDetail/storeDetail.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('qupeyApp')
-  .controller('StoreDetailCtrl', function ($scope, $stateParams, store, $modal) {
+  .controller('StoreDetailCtrl', function ($scope, $stateParams, store, $modal, Auth, Customer) {
     
+    var user = Auth.getCurrentUser(); 
+    var userId = Auth.getCurrentUser()._id; 
+
     console.log($stateParams);
     store.getOne($stateParams.id)
     	.success(function(s){
@@ -13,13 +16,23 @@ angular.module('qupeyApp')
     		console.log(err);
     	})
 
+    // add qupey to users qupey array 
     $scope.openUseMyselfModal = function() {
-        console.log("got to open modal")
-        $modal.open({
-            templateUrl: 'app/storeDetail/useMyselfModal.html',
-            controller: 'UseMyselfModalCtrl',
-            scope: $scope
-        });
+        // on this object 
+        // console.log('id: ', userId, 'scope: ', $scope.store.default_qupey._id)
+        Customer.addMyQupey(userId, $scope.store.default_qupey._id)
+        .then(function success(data){
+            console.log('data: ', data)
+            console.log("got to open modal")
+            $modal.open({
+                templateUrl: 'app/storeDetail/useMyselfModal.html',
+                controller: 'UseMyselfModalCtrl',
+                scope: $scope
+            });
+        })
+        .then(null, function(err){
+            console.log('err: ', err)
+        }) 
     }
 
     $scope.openShareModal = function() {
