5be91b9bd8a3d178731473c3105be86ea9e4d461
Ayana <ayana.d.i.wilson@gmail.com>
2015-04-20 18:13:34 -0400

    refactored by accessing store through prototypical scope inheritance, not via parent scope

--- client/app/storeDetail/shareModal/shareModal.controller.js
+++ client/app/storeDetail/shareModal/shareModal.controller.js
@@ -2,35 +2,42 @@
 
 angular.module('qupeyApp')
   .controller('ShareModalCtrl', function ($scope, $modalInstance, Auth, Customer, $stateParams) {
-   		Auth.getCurrentUser().$promise.then(function(user){
-   			console.log('user: ', user)
-   			$scope.user = user;
+ 		Auth.getCurrentUser().$promise.then(function(user){
+ 			console.log('user: ', user)
+ 			$scope.user = user;
+      $scope.userId = user._id; 
+ 		})
+ 		.catch(function(err){
+ 			console.log(err);
+ 		})
 
-        $scope.userId = user._id; 
-   		})
-   		.catch(function(err){
-   			console.log(err);
-   		})
-
-   		console.log($modalInstance);
+   	console.log($modalInstance);
 		$scope.thing = "FDSAFDSAFDSAFDSAFDAS"
 
 		$scope.backToBrowsing = function(){
-    		$modalInstance.close();
-    	}
+    	$modalInstance.close();
+    }
 
-      $scope.emails = []; 
-        $scope.shareQupey = function(){
-          console.log('scope: ', $scope.emails)
-          // grab store object from parent scope 
-          Customer.shareQupey($scope.userId, $scope.emails, $scope.$parent.store)
-          .then(function success(){
-            $modalInstance.close()
-          })
-          .then(null, function(err){
-            console.log('err: ', err)
-            $modalInstance.close
-          })
-        }
+    $scope.addContact = function(contact){
+      console.log('contact: ', contact)
+      // console.log('parent: ', scope.$parent)
+      $scope.emails.push(contact.email)
+    }
+
+    console.log('store on current scope: ', $scope.store)
+
+    $scope.emails = []; 
+    $scope.shareQupey = function(){
+      console.log('scope: ', $scope.emails)
+      // grab store object from parent scope 
+      Customer.shareQupey($scope.userId, $scope.emails, $scope.$parent.store)
+      .then(function success(){
+        $modalInstance.close()
+      })
+      .then(null, function(err){
+        console.log('err: ', err)
+        $modalInstance.close
+      })
+    }
 
   });
